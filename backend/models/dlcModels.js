03dcf202b80b29c3f5618cf2376add0656edcbfb
zidane-arkan <zidark897@Gmail.com>
2022-11-16 08:57:27 +0700

    Add dlcModels

--- backend/models/dlcModels.js
+++ backend/models/dlcModels.js
@@ -3,5 +3,15 @@ import db from '../database/database.js';
 
 const { DataTypes } = Sequelize;
 
+const Dlc = db.define('dlc', {
+    nama_dlc: {
+        type: DataTypes.STRING(25), allowNull: false
+    },
+    tanggal_pembaruan: {
+        type: DataTypes.DATEONLY, allowNull: false
+    },
+}, {
+    freezeTableName: true
+});
 
-
+export default Dlc;
